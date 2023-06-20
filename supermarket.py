64f8c9dd8713550581f0a32a9e576da886388b84
MuffinHavlop <101418643+MuffinHavlop@users.noreply.github.com>
2023-06-20 19:10:06 +0700

    Update supermarket.py
    added some features

--- supermarket.py
+++ supermarket.py
@@ -4,45 +4,39 @@ utensils = {"fork": 2, "knife": 5, "spoon": 3}
 
 goods = ["vegetables: ", "meat: ", "utensils: "]
 
-def display_goods():
-    global vegetables
-    global meat
-    global utensils
-    round = 0   
-    print(goods[round])
-    for key, value in vegetables.items():
-        print("+", key, ":", value, "$")
-    round += 1 
-    print(goods[round])
-    for key, value in meat.items():
-        print("+", key, ":", value, "$")
-    round += 1
-    print(goods[round])
-    for key, value in utensils.items():
-        print("+", key, ":", value, "$")
-    round += 1   
-    
-def buying():
-    global vegetables
-    global meat
-    global utensils
-    order = input("Enter what you want to buy: ")
-    order = order.lower()
-    quantity = input("How many of them: ")
-    if order in vegetables:
-        vegetable_price = (vegetables[order])
-        total_bill = int(quantity) * int(vegetable_price)
-        print(f"The total bill for {quantity} {order} is: {total_bill}$")
-    elif order in meat:
-        meat_price = (meat[order])
-        total_bill = int(quantity) * int(meat_price)
-        print(f"The total bill for {quantity} {order} is: {total_bill}$")     
-    else:
-        utensils_price = (utensils[order])
-        total_bill = int(quantity) * int(utensils_price)
-        print(f"The total bill for {quantity} {order} is: {total_bill}$")  
-        
-
 while True:
-    display_goods()
-    buying()
+    order = input("Enter what you want to buy, or re-check the menu(RC): ")
+    order = order.lower()
+    if order == "rc":          
+        round = 0   
+        print(goods[round])
+        for key, value in vegetables.items():
+            print("+", key, ":", value, "$")
+        round += 1 
+        print(goods[round])
+        for key, value in meat.items():
+            print("+", key, ":", value, "$")
+        round += 1
+        print(goods[round])
+        for key, value in utensils.items():
+            print("+", key, ":", value, "$")
+        round += 1   
+    else:       
+        quantity = input("How many of them: ")
+        if order in vegetables:
+            vegetable_price = (vegetables[order])
+            total_bill = int(quantity) * int(vegetable_price)
+            print(f"The total bill for {quantity} {order} is: {total_bill}$")
+        elif order in meat:
+            meat_price = (meat[order])
+            total_bill = int(quantity) * int(meat_price)
+            print(f"The total bill for {quantity} {order} is: {total_bill}$")     
+        else:
+            utensils_price = (utensils[order])
+            total_bill = int(quantity) * int(utensils_price)
+            print(f"The total bill for {quantity} {order} is: {total_bill}$")  
+        
+    
+    
+                
+
